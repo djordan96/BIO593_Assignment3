aa3450f3471dd1287d4dc5c38843c5cf775d6661
Kevin Wong <kevinhwong1@gmail.com>
2018-03-30 12:16:44 -0400

    adding scripts

--- Untitled.R
+++
@@ -1,3 +0,0 @@
-download.file("https://ndownloader.figshare.com/files/2292169" , "data/portal_data_joined.csv") 
-download.file("https://ndownloader.figshare.com/files/2292171" , "data/portal_data.sqlite", mode = "wb")
-surveys <- read.csv("data/portal_data_joined.csv") #uploading csv
---
+++ scripts/Hypothesis_3.R
@@ -0,0 +1,41 @@
+#Hypothesis 3
+surveys <- read.csv("data/portal_data_joined.csv") #uploading csv
+surveys <- read.csv("~/Projects/BIO593_Assignment3/data/portal_data_joined.csv") #uploading csv
+
+library(dbplyr)
+library(RSQLite)
+library(tidyverse)
+
+surveys_specplot<- surveys %>% 
+  select(taxa, plot_type, species) %>%
+  filter(taxa == "Rodent") %>% 
+  filter(!is.na(taxa), !is.na(plot_type), !is.na(species)) %>%
+  group_by(plot_type, species) %>%
+  tally()
+
+
+ggplot(data = surveys_specplot, aes(x = factor(-n), y = n, fill = species)) +
+  geom_bar(stat="identity") +
+  ylab("Counts") + xlab("Species") + 
+  guides(fill = guide_legend(ncol = 3)) +
+  facet_wrap(~ plot_type, scales = "free_x", drop = TRUE) +
+  scale_fill_discrete(name="Species") +
+  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank()) +
+  theme(legend.position=c(0.85,0.25)) 
+
+
+
+#stats
+#factors: ind- Plot type, ind- Species ID, dep- abundance 
+#assumptions 
+
+#normality
+
+#homogenity of variance
+
+#2 way ANOVA 
+anova(lm(data = surveys_specplot, n ~ plot_type * species))
